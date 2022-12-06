4ed0fa36750b87b9b4ab45f26bd670ff88ba92be
filippo benvenuti <benvenuti.filippo.99@gmail.com>
2022-12-06 17:04:32 +0100

    private methods

--- whatsapp.py
+++ whatsapp.py
@@ -1,49 +1,52 @@
-import wx 
-import wx.html2 
+import wx
+import wx.html2
 
-class MyBrowser(wx.Dialog): 
-  def __init__(self, *args, **kwds): 
-    wx.Dialog.__init__(self, *args, **kwds) 
+class WhatsappBOT(wx.Dialog):
+  def __init__(self, *args, **kwds):
+    wx.Dialog.__init__(self, *args, **kwds)
     sizer = wx.BoxSizer(wx.VERTICAL) # GUI
-    self.browser = wx.html2.WebView.New(self) 
-    sizer.Add(self.browser, 1, wx.EXPAND, 10) # GUI
-    self.Bind(wx.html2.EVT_WEBVIEW_LOADED, self.onWebViewLoaded, self.browser)
-    self.browser.AddScriptMessageHandler('toPyt')
-    self.Bind(wx.html2.EVT_WEBVIEW_SCRIPT_MESSAGE_RECEIVED, self.onMessage, self.browser)
+    self.__browser = wx.html2.WebView.New(self)
+    sizer.Add(self.__browser, 1, wx.EXPAND, 10) # GUI
+    self.Bind(wx.html2.EVT_WEBVIEW_LOADED, self.__onWebViewLoaded, self.__browser)
+    self.__browser.AddScriptMessageHandler('toPyt')
+    self.Bind(wx.html2.EVT_WEBVIEW_SCRIPT_MESSAGE_RECEIVED, self.__onMessage, self.__browser)
     self.SetSizer(sizer)  # GUI
     self.SetSize((900, 700)) # GUI
 
     button = wx.Button(self, wx.ID_ANY, 'Test', (10, 10)) # GUI
     sizer.Add(button, 1, wx.SHAPED, 10) # GUI
-    button.Bind(wx.EVT_BUTTON, self.onButton) # GUI
+    button.Bind(wx.EVT_BUTTON, self.__onButton) # GUI
 
-  def js(self, script):
-    success, result = self.browser.RunScript(self.act + '\n' + script)
+  def __js(self, script):
+    success, result = self.__browser.RunScript(self.__act + '\n' + script)
     return result if success else None
-  def jsAsync(self, script):
-    self.browser.RunScriptAsync(self.act + '\n' + script)
+  def __jsAsync(self, script):
+    self.__browser.RunScriptAsync(self.__act + '\n' + script)
 
-  def onMessage(self, message):
+  def __onMessage(self, message):
     print(message.GetString())
 
-  def onWebViewLoaded(self, evt):
+  def __onWebViewLoaded(self, evt):
     # Actions enabler.
     file = open('whatsappAct.js',mode='r')
-    self.act = file.read()
+    self.__act = file.read()
     file.close()
-    self.jsAsync('baseSetup()')
+    self.__jsAsync('baseSetup()')
 
-  def onButton(self, evt):  # GUI
+  def __onButton(self, evt):  # GUI
     #self.js('sendMessage("ottimo")')
     #print(self.js('getAllMessages()'))
     #print(self.js('getAllChats()'))
     #self.js('selectChat("Io")')
     #print(self.js('getLastMessage()'))
-    #self.js('send("ciao", "bella")')
+    self.js('send("ciao", "bella")')
 
-if __name__ == '__main__': 
-  app = wx.App() 
-  dialog = MyBrowser(None, -1)
-  dialog.browser.LoadURL("https://web.whatsapp.com")
-  dialog.Show() # GUI
+  def loadWhatsapp(self):
+    self.__browser.LoadURL("https://web.whatsapp.com")
+
+if __name__ == '__main__':
+  app = wx.App()
+  whats = WhatsappBOT(None, -1)
+  whats.loadWhatsapp()
+  whats.Show() # GUI
   app.MainLoop()
