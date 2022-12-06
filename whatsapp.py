0fca94b00b9b728880baad05128e44bcb4e615d9
filippo benvenuti <benvenuti.filippo.99@gmail.com>
2022-12-06 16:05:40 +0100

    marked GUI lines

--- whatsapp.py
+++ whatsapp.py
@@ -4,25 +4,22 @@ import wx.html2
 class MyBrowser(wx.Dialog): 
   def __init__(self, *args, **kwds): 
     wx.Dialog.__init__(self, *args, **kwds) 
-    sizer = wx.BoxSizer(wx.VERTICAL) 
+    sizer = wx.BoxSizer(wx.VERTICAL) # GUI
     self.browser = wx.html2.WebView.New(self) 
-    sizer.Add(self.browser, 1, wx.EXPAND, 10) 
+    sizer.Add(self.browser, 1, wx.EXPAND, 10) # GUI
     self.Bind(wx.html2.EVT_WEBVIEW_LOADED, self.onWebViewLoaded, self.browser)
     self.browser.AddScriptMessageHandler('toPyt')
     self.Bind(wx.html2.EVT_WEBVIEW_SCRIPT_MESSAGE_RECEIVED, self.onMessage, self.browser)
-    self.SetSizer(sizer) 
-    self.SetSize((900, 700))
+    self.SetSizer(sizer)  # GUI
+    self.SetSize((900, 700)) # GUI
 
-    button = wx.Button(self, wx.ID_ANY, 'Test', (10, 10))
-    sizer.Add(button, 1, wx.SHAPED, 10)
-    button.Bind(wx.EVT_BUTTON, self.onButton)
+    button = wx.Button(self, wx.ID_ANY, 'Test', (10, 10)) # GUI
+    sizer.Add(button, 1, wx.SHAPED, 10) # GUI
+    button.Bind(wx.EVT_BUTTON, self.onButton) # GUI
 
   def js(self, script):
     success, result = self.browser.RunScript(self.act + '\n' + script)
-    if success:
-      return result
-    else:
-      return None
+    return result if success else None
   def jsAsync(self, script):
     self.browser.RunScriptAsync(self.act + '\n' + script)
 
@@ -34,29 +31,19 @@ class MyBrowser(wx.Dialog):
     file = open('whatsappAct.js',mode='r')
     self.act = file.read()
     file.close()
-    # Events subscribing.
-    #file = open('whatsappEvt.js',mode='r')
-    #self.js(file.read())
-    #file.close()
-    #
-    #self.js('sendMessage("ottimo")')
-    #self.js('getAllMessages()')
-    #self.js('getAllChats()')
-    #self.js('selectChat("Io")')
-    print("baseSetup")
     self.jsAsync('baseSetup()')
 
-  def onButton(self, evt):
+  def onButton(self, evt):  # GUI
     #self.js('sendMessage("ottimo")')
     #print(self.js('getAllMessages()'))
     #print(self.js('getAllChats()'))
     #self.js('selectChat("Io")')
     #print(self.js('getLastMessage()'))
-    self.js('send("ciao", "bella")')
+    #self.js('send("ciao", "bella")')
 
 if __name__ == '__main__': 
   app = wx.App() 
   dialog = MyBrowser(None, -1)
   dialog.browser.LoadURL("https://web.whatsapp.com")
-  dialog.Show()
+  dialog.Show() # GUI
   app.MainLoop()
