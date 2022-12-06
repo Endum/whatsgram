8a1c50a4d282716e81e3169b3ddbd7cb0cee00a6
filippo benvenuti <benvenuti.filippo.99@gmail.com>
2022-12-06 22:33:28 +0100

    Welcome messages and agents creation

--- whatsgram.py
+++ whatsgram.py
@@ -2,18 +2,37 @@ from whatsapp import WhatsappBOT
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+async def sendMessages(update, context, messages):
+  for message_part in messages:
+    await context.bot.send_message(
+      chat_id=update.effective_chat.id,
+      text=message_part
+    )
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  message = [
+  messages = [
     "HI!",
     "I'm Whatsgram!",
     "My job here is to merge groups between Telegram and Whatsapp.",
-    "To start, make me join the Telegram group you want to merge.",
+    "To start, make me join the group you want to merge with a Whatsapp group.",
     "Make sure to have another device ready to scan whatsapp QRcode.",
   ]
-  await context.bot.send_message(
-    chat_id=update.effective_chat.id,
-    text=message_part
-  )
+  await sendMessages(update, context, messages)
+
+async def new_member(update, context):
+  messages = [
+    "HI!",
+    "Be ready to scan QR code!!",
+    "A Whatsgram agent is connecting... as soon as he has it, he'll send you the QR.",
+    "If it expires, you'll need to kick and make me rejoin this group.",
+    "I'll now leave you in the hands of my agent!",
+    "Thanks for using whatsgram! :D"
+  ]
+  for member in update.message.new_chat_members:
+    if member.username == 'whatsgram_bot':
+      await sendMessages(messages)
+
+  # Call for a whatsgram agent.
 
 if __name__ == '__main__':
   file = open('token.txt',mode='r')
@@ -22,6 +41,8 @@ if __name__ == '__main__':
 
   start_handler = CommandHandler('start', start)
   application.add_handler(start_handler)
+  welcome_handler = MessageHandler(Filters.status_update.new_chat_members, new_member)
+  application.add_handler(welcome_handler)
   
   print("Bot up and running :)")
 
