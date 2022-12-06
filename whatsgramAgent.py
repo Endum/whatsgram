18a172559c57330039cf9ac8700180a1f1bee163
filippo benvenuti <benvenuti.filippo.99@gmail.com>
2022-12-06 22:33:52 +0100

    Just a description of what to do

---
+++ whatsgramAgent.py
@@ -0,0 +1,5 @@
+# Here, a new istance of whatsgram bot.
+# First of all should start the setup of whatsapp.
+# When finished, should start listening for messages in specified groups.
+# Information about telegram group should be passed in creation.
+# Information about whatsapp group are gathered during the setup.
