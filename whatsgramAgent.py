e48a4f58e877fe36378e69b94137fec05c95b8d9
Filippo Benvenuti <filippo.benvenuti3@studio.unibo.it>
2022-12-16 11:10:07 +0100

    added arg parser

--- whatsgramAgent.py
+++ whatsgramAgent.py
@@ -3,3 +3,13 @@
 # When finished, should start listening for messages in specified groups.
 # Information about telegram group should be passed in creation.
 # Information about whatsapp group are gathered during the setup.
+
+import argparse
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('telegramToken')
+	parser.add_argument('telegramChatID')
+	args = parser.parse_args()
+	
+	
